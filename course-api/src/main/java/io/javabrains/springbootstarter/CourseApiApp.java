5f4c9b3231c7a611bc3fc86e7989f30d7e2ab0f9
aniketrb-github <aniketbharsakale@gmail.com>
2019-02-03 14:12:29 +0530

    adding 1st basic maven demo for spring boot

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,17 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This was a Maven Project which was converted to a Spring Boot Project
+ * @author Aniket.Bharsakale
+ */
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
