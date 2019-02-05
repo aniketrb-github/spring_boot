ebe64ee7d900fa6ceead6ac1822830febd8c6842
aniketrb-github <aniketbharsakale@gmail.com>
2019-02-06 01:04:24 +0530

    added custom app exception class

---
+++ project-portal-mgt/src/main/java/org/arb_tech/web/exception/ProjectPortalException.java
@@ -0,0 +1,10 @@
+package org.arb_tech.web.exception;
+
+public class ProjectPortalException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 772193746001025060L;
+
+}
