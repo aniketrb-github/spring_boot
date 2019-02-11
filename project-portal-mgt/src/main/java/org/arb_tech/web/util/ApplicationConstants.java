5d91f9b14b167d1e8b7bdde6294f672077f04028
aniketrb-github <aniketbharsakale@gmail.com>
2019-02-12 00:32:34 +0530

    added application constant file

---
+++ project-portal-mgt/src/main/java/org/arb_tech/web/util/ApplicationConstants.java
@@ -0,0 +1,15 @@
+package org.arb_tech.web.util;
+
+/**
+ * contains the String Literals, Numeric Constants and other frequently used values 
+ * @author Aniket.Bharsakale
+ */
+public class ApplicationConstants {
+
+	public enum ACTION {
+		GET_ALL_PROJECTS, GET_PROJECT, CREATE_PROJECT,
+		GET_ALL_EMPLOYEES, GET_EMPLOYEE, UPDATE_EMPLOYEE, CREATE_EMPLOYEE,
+		GET_ALL_TASKS, GET_TASK, CREATE_TASK,
+		GET_ALL_BUGS, GET_BUG, CREATE_BUG, UPDATE_BUG;
+	}
+}
