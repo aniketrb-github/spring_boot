39d9a7487ba5b84be9916e5b847ea74711ad4348
aniketrb-github <aniketbharsakale@gmail.com>
2019-03-03 02:54:00 +0530

    added literals & local messages

--- project-portal-mgt/src/main/java/org/arb_tech/web/util/Messages.java
+++ project-portal-mgt/src/main/java/org/arb_tech/web/util/Messages.java
@@ -5,6 +5,9 @@ public class Messages {
 	public static final String MSG_UNEXPECTED_ERROR = "response.unexpected.error";
 	public static final String MSG_USER_NOT_FOUND = "response.user.not.found";
 	public static final String MSG_TRANSACTION_FAILED = "response.transaction.failed";
-	
-	public static final String MSG_PROJECT_NOT_FOUND = "response.project.not.found";
+
+	public static final String PROJECT_NOT_FOUND = "response.project.not.found";
+	public static final String PROJECT_DELETE_SUCCESS = "response.project.deleted.success";
+	public static final String PROJECT_VO_NULL = "error.project.vo.null";
+	public static final String PROJECT_ID_NULL = "error.project.id.null";
 }
