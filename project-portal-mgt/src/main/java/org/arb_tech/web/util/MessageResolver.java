66e2ed659a78b704aee897dccfecaa78839f72f1
aniketrb-github <aniketbharsakale@gmail.com>
2019-03-02 21:50:58 +0530

    added local message resolver component

---
+++ project-portal-mgt/src/main/java/org/arb_tech/web/util/MessageResolver.java
@@ -0,0 +1,21 @@
+package org.arb_tech.web.util;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageResolver {
+	
+	@Autowired
+	private MessageSource messageSource;
+
+	public String resolveLocalizedMessage(String messageCode) {
+		Locale currentLocale = LocaleContextHolder.getLocale();
+		String localizedErrorMessage = messageSource.getMessage(messageCode, new Object[] {}, currentLocale);
+		return localizedErrorMessage;
+	}
+}
