5b361be11558fe31418a231bb3a58141f8dbdcae
aniketrb-github <aniketbharsakale@gmail.com>
2019-03-03 05:52:39 +0530

    added URI component identifiers to constants list

--- project-portal-mgt/src/main/java/org/arb_tech/web/util/ApplicationConstants.java
+++ project-portal-mgt/src/main/java/org/arb_tech/web/util/ApplicationConstants.java
@@ -14,12 +14,21 @@ public class ApplicationConstants {
 		GET_ALL_BUGS, GET_BUG, CREATE_BUG, UPDATE_BUG;
 	}
 	
-	// CONTROLLER PATH
+	// CONTROLLER PATHS
 	public static final String EMPLOYEES = "/employees"; 
 	public static final String BUGS = "/bugs";
 	public static final String PROJECTS = "/projects";
 	public static final String TASKS = "/tasks";
+	
+	// URI COMPONENTS
 	public static final String PATH_VAR_ID = "/{id}";
+	public static final String REQUEST_PARAM_ID = "id";
+	
+	// IDENTIFIERS
+	public static final String STATUS_ID = "statusId";
+	public static final String REPORTER_ID = "reporterId";
+	public static final String ASSIGNEE_ID = "assigneeId";
+	public static final String PROJECT_CODE = "projectCode";
 	
 	public static final String UTF8_CHARSET_NAME= "UTF-8";
 	public static final String MESSAGES = "messages";
